c4fdf6bd180d124d9ae47a9d0953d7cb15bfac2a
mila <psyworld>
2017-12-11 00:36:27 +0300

    total fix :D

--- iteration_method_sys.py
+++ iteration_method_sys.py
@@ -1,7 +1,7 @@
 import math as m
 import numpy as np
 
-eps = 0.001
+n = 4
 
 def main():
 	a = np.array([[4, 2, -1, 0.5], 
@@ -9,51 +9,65 @@ def main():
 		 [2, 1, -4, -1.5],
 		 [1, -0.4, 0.8, -3]])
     
-	free = [11.5, 1, 0.5, 7.2]
-	n = 4
-	norm = round(getNorm(a), 4)
-	alpha, beta = start(n, a, free)
-	print(alpha, beta)
-	x = iteration(n, alpha, beta)
-	print(x)
+	b = [11.5, 1, 0.5, 7.2]
+
+	epsilon = 0.001
+
+	x = iteration(a, b, epsilon)
+	print(x, end='\n\n')
 	print(np.dot(a, x))
 
-def getNorm(array):
-	return sum(array[i][j]**2 for i in range(len(array)) 
-							  for j in range(len(array)))**(0.5)
 
-def start(n, array, free):
-	b = np.zeros((n, 1))
-	a = np.zeros((n, n))
+def eval(A, b, X):
+	e = np.zeros(n)
 
 	for i in range(n):
-		b[i] = round(free[i]/array[i][i], 4)
+		e[i] = -b[i]
 		for j in range(n):
-			if i != j:
-				a[i][j] = round(-array[i][j]/array[i][i], 4)
-			else:
-				a[i][j] = 0
-	return a, b
- 
-def iteration(n, a, b):
-	x = np.zeros((n,1))
-	for i in range(n):
-			x[i] = b[i] + sum(a[i][j]*x[j] for j in range(n))
-	i = 1
-	x_ = np.zeros((n,1))
-	while True:
-		for i in range(n):
-			x[i] = b[i] + sum(a[i][j]*x[j] for j in range(n))
-		if not epsilon(x, x_, eps):
-			break
-		x_ = x
-	return x	
+			e[i] += A[i][j]*X[j]
+	return e
 
-def epsilon(x, x_, eps):
-	for i in range(len(x)):
-		if m.fabs(x[i] - x_[i]) >= eps:
-			return True
-	return False
+def eps(A, b, epsilon):
+	for i in range(n):
+		if m.fabs(A[i] - b[i]) >= epsilon:
+			return False
+	return True
+
+def iteration_pass(A, b, X_):
+	x = np.zeros(n)
+
+	for i in range(n):
+		acc = 0
+		for j in range(n):
+			if j != i:
+				acc += A[i][j]*X_[j]
+		x[i] = (b[i]-acc)/A[i][i]
+
+	return x
+
+def iteration(A, b, epsilon):
+	y = b
+	x_ = b
+	x = b
+	x_ = x_
+	x = iteration_pass(A, b, x_)
+	while (not eps(x_, x, epsilon)):
+		x_ = x
+		x = iteration_pass(A, b, x_ )
+
+	add_pass = True
+	while (add_pass):
+		add_pass = False
+
+		e = eval(A, b, x)
+		for i in e:
+			if m.fabs(i) > epsilon:
+				add_pass = True
+
+		if add_pass:
+			x = iteration_pass(A, b, x)
+
+	return x
 
 if __name__ == '__main__':
 	main()
